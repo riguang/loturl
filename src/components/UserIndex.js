f2af57a37ba6a1df80a412d13b91beb257dfadb1
riguang <1029367878@qq.com>
2017-10-08 22:17:12 +0800

    新增收藏夹添加按钮

--- src/components/UserIndex.js
+++ src/components/UserIndex.js
@@ -1,6 +1,6 @@
 import React, { Component} from 'react';
 import '../css/UserIndex.less';
-import { Layout} from 'antd';
+import { Layout,Input,Button} from 'antd';
 import URLTable from './userIndex/URLTable';
 import MenuItem from './userIndex/MenuItem';
 import Store from './userIndex/Store';
@@ -13,7 +13,9 @@ class UserIndex extends Component {
   constructor(props){
     super(props);
     this.state ={
-      selectedKey:1
+      selectedKey:1,
+      inputName:"",
+      selectedKeys:['1']
     }
   };
  
@@ -22,12 +24,33 @@ class UserIndex extends Component {
     this.setState({selectedKey});
     Store.favorite._selectKey=Object.keys(Store.favorite)[selectedKey-1];
   };
+  addFavorite = () =>{
+    const inputName = this.state.inputName;
+    if(inputName !=="")
+    {
+      Store.favorite[inputName]=[];
+      this.setState({
+        inputName:""
+      });//触发render才会刷新
+    }
+  }
   render() {
+    const {inputName,selectedKeys} = this.state;
     return (
         <Layout>
         <Sider style={{ overflow: 'auto', height: '100vh', position: 'fixed', left: 0}}>
           <div className="logo" />
-          <MenuItem ClassName={Object.keys(Store.favorite)} UpdateSelectedKey={this.UpdateSelectedKey.bind(this)} />
+          <MenuItem ClassName={Object.keys(Store.favorite)} 
+          UpdateSelectedKey={this.UpdateSelectedKey.bind(this)}
+           />
+          <div className="example-input">
+              <Input size="default size" placeholder="请输入收藏夹名称" value={inputName} onChange={(v)=>this.setState({inputName:v.target.value})} />
+              <Button type="primary" 
+              className="btn-addFavorite" 
+              style={{width: "100%"}}
+              onClick={this.addFavorite}
+              >添加收藏夹</Button>
+          </div>
         </Sider>
         <Layout style={{ marginLeft: 200 }}>
           <Header style={{ background: '#fff', padding: 0 }} />
@@ -37,7 +60,7 @@ class UserIndex extends Component {
             </div>
           </Content>
           <Footer style={{ textAlign: 'center',backgroundColor:"#fff" }}>
-            LotURL ©2017 Created by Shine
+            LotURL ©2017 Created by SunShine
           </Footer>
         </Layout>
       </Layout>
--- src/components/userIndex/MenuItem.js
+++ src/components/userIndex/MenuItem.js
@@ -5,18 +5,17 @@ export default class MenuItem extends Component{
     static defaultProps={
       ClassName:["默认收藏"]
     }
-  
     render(){
       const {ClassName} = this.props;
       return (
           <Menu theme="light" 
           mode="inline" 
-          style={{height: '100vh'}} 
+          style={{height: 'calc(100vh - 56px)'}} 
           defaultSelectedKeys={['1']}
           onSelect={({ item, key, selectedKeys })=>this.props.UpdateSelectedKey(key)}
           >
             {
-              ClassName.map((value,index)=>
+              ClassName.map((value,index)=> value !=="_selectKey" &&
               <Menu.Item key={index+1}>
                   <span className="nav-text">{value}</span>
               </Menu.Item>
